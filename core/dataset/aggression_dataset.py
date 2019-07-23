1747e61464181ccf3fe886b99f2ce3671d7ed6e2
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-07-23 10:06:01 +0800

    [Dataset] rename to aggresseion dataset for sake of naming convention

--- core/dataset/aggression_dataset.py
+++ core/dataset/aggression_dataset.py
@@ -1,10 +1,9 @@
 import os
 from PIL import Image
 
-from torch.utils.data import Dataset
 from torchvision import transforms
 
-__all__ = ['AggressionDataset']
+from core.dataset.base_dataset import PathLabelPairDataset
 
 
 def clean(path_label_pairs):
@@ -16,7 +15,7 @@ def clean(path_label_pairs):
     return cleaned
 
 
-class AggressionDataset(Dataset):
+class AggressionDataset(PathLabelPairDataset):
 
     def __init__(self, opts):
         """Dataset wrapper for Approximated Rank Pool images.
@@ -27,34 +26,25 @@ class AggressionDataset(Dataset):
             - input_mean (arr): input mean in array-like/tuples, must has same length as the input channels(e.g RGB: 3 channels)
             - input_std (arr): input standard dev in array-like/tuples, must has same length as the input channels (e.g RGB: 3 channels) 
         """
-        self.filepath = opts.filepath
-
-        if not os.path.exists(os.path.abspath(self.filepath)):
-            raise FileNotFoundError("path-label-pairs file (.txt) not found, %s" % filepath)
-
+        super(AggressionDataset, self).__init__(opts)
+        self.filepath = opts.pathlabel_pair_file
+        AggressionDataset.check_path_label_pair_file(self.filepath)
+        
         # read files and get file paths (dataset)
         with open(self.filepath, 'r') as fp:
             self.path_label_pairs = fp.readlines()
         
         self.path_label_pairs = clean(self.path_label_pairs)
 
-        # setup attributes
-        self.preprocessing_filters = preprocessings
-        C, H, W = input_size
-        self.input_size = (H, W)
-        self.input_mean = input_mean
-        self.input_std = input_std
-
         # prepare image preprocessings and transforms
         self.transforms = transforms.Compose([
-                                        transforms.Resize(self.input_size),
-                                        transforms.CenterCrop(self.input_size),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(self.input_mean, self.input_std)
-                                        ])
+            transforms.Resize(self.opts.input_size),
+            transforms.CenterCrop(self.opts.input_size),
+            transforms.ToTensor(),
+            transforms.Normalize(self.opts.input_means, self.opts.input_std)
+        ])
 
     def __getitem__(self, i):
-        assert len(self.path_label_pairs[i].strip().split()) == 2 
         path, label = self.path_label_pairs[i].strip().split()
         img = Image.open(path)
         img = self.transforms(img)
