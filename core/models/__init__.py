41f226ad18b839a35ef7467052c7985c27876b89
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-07-23 10:07:41 +0800

    [Models] fix module importing

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -18,7 +18,7 @@ See our template model class 'template_model.py' for more details.
 """
 
 import importlib
-from models.base_model import BaseModel
+from core.models.base_model import BaseModel
 
 
 def find_model_using_name(model_name):
@@ -28,7 +28,7 @@ def find_model_using_name(model_name):
     be instantiated. It has to be a subclass of BaseModel,
     and it is case-insensitive.
     """
-    model_filename = "models." + model_name + "_model"
+    model_filename = "core.models." + model_name + "_model"
     modellib = importlib.import_module(model_filename)
     model = None
     target_model_name = model_name.replace('_', '') + 'model'
