9d93f6ecf834896b0455a9aeb8fd5bceb32998b5
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-07-23 10:04:19 +0800

    [CLI] main tests.py using new concept

--- test.py
+++ test.py
@@ -12,8 +12,9 @@ from torchsummary import summary
 
 import pretrainedmodels
 
-from core.dataset import AggressionDataset
-
+from core.models import create_model
+from core.dataset import create_dataset
+from core.options.train_options import TrainOptions
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SPLITS_DIR = os.path.join(BASE_DIR, "data/splits/")
@@ -25,182 +26,13 @@ def update_learning_rate(optimizer, learning_rate):
         param_group['lr'] = learning_rate
 
 
-num_class = 2
-model = pretrainedmodels.__dict__['resnext101_32x4d'](
-    num_classes=1000, pretrained='imagenet'
-)
+opts = TrainOptions().parse()
+# dataset = create_dataset(opts)
+# dataset_size = len(dataset)
+# print("The number of training images: %d" % dataset_size)
 
-# last fully-connected linear module settings
-model.last_layer_name = 'last_linear'
-num_feats = model.last_linear.in_features
-model.last_linear = torch.nn.Linear(num_feats, num_class)
-
-# last fully-connected linear module settings
-model.last_layer_name = 'last_linear'
-num_feats = model.last_linear.in_features
-model.last_linear = torch.nn.Linear(num_feats, num_class)
-
-# image settings
-input_size = (3, 224, 224)
-input_mean = (0.485, 0.456, 0.406)
-input_std = (0.229, 0.224, 0.225)
-
-# hyperparams settings
-epochs = 20
-batch_size = 32 # mini-batch-size
-learning_rate = 0.01
-momentum = 0.5
-decay_factor = 0.35
-decay_rate = 5 # in epochs
-eval_freq = 3 # in epochs
+model = create_model(opts)
+model.prepare_model()
+total_iters = 0
 
 
-# splits file
-ftrain_split = os.path.join(SPLITS_DIR, "aggression_train_split_0.txt")
-fval_split = os.path.join(SPLITS_DIR, "aggression_val_split_0.txt")
-ftest_split = os.path.join(SPLITS_DIR, "aggression_test_split.txt")
-
-# data generator settings: dataset and dataloader
-train_dataset = AggressionDataset(ftrain_split, input_size,
-    input_mean=input_mean, input_std=input_std)
-val_dataset = AggressionDataset(fval_split, input_size,
-    input_mean=input_mean, input_std=input_std)
-test_dataset = AggressionDataset(ftest_split, input_size,
-    input_mean=input_mean, input_std=input_std)
-
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
-if torch.cuda.is_available():
-    model.cuda()
-
-# Loss and backprop settings
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(
-    model.parameters(),
-    lr=learning_rate,
-    momentum=momentum
-)
-
-# printing to widget
-print("----------------------------------------------------------------------------------------")
-print("TRAINING SESSION:")
-print("Model: %s" % model.__class__.__name__)
-print("Dataset: %s" % AggressionDataset.__name__)
-
-print("=======================================")
-print("HYPERPARAMS: ")
-print("Epochs: %d" % epochs)
-print("Batch-size: %d" % batch_size)
-print("Initial learning rate: %s" % learning_rate)
-
-print("========================================")
-print("SUMMARY")
-print("%s" % summary(model, input_size))
-
-print("BEGIN: %s" % time.time())
-
-
-best_acc = 0.0
-
-running_loss = 0.0
-running_acc = 0
-
-# trainload
-dataiter = iter(train_loader)
-X_train, labels = next(dataiter)
-
-print("Input Train X shape: ", X_train.shape, labels.shape)
-
-# training
-if torch.cuda.is_available():
-    X_train = torch.autograd.Variable(X_train.cuda())
-    labels = torch.autograd.Variable(labels.cuda())
-model.train()
-outputs = model(X_train)
-loss = criterion(outputs, labels)
-
-optimizer.zero_grad()
-loss.backward()
-optimizer.step()
-
-_, preds = torch.max(outputs.data, 1)
-acc = torch.mean((preds == labels.data).float())
-
-learning_rate *= decay_factor
-update_learning_rate(optimizer, learning_rate)
-
-print("-- Training Once | Acc: {} Loss: {} ".format(acc, loss.data))
-
-
-# valload
-dataiter = iter(val_loader)
-X_val, labels_val = next(dataiter)
-
-print("Input Val X shape: ", X_val.shape, labels_val.shape)
-
-# validation
-if torch.cuda.is_available():
-    X_val = torch.autograd.Variable(X_val.cuda())
-    labels_val = torch.autograd.Variable(labels_val.cuda())
-
-with torch.no_grad():
-    model.eval()
-    val_outputs = model(X_val)
-    val_loss = criterion(outputs, labels_val)
-
-    _, val_preds = torch.max(val_outputs.data, 1)
-    val_acc = torch.mean((val_preds == labels_val.data).float())
-
-    print("-- Validation Once | Acc: {} Loss : {}".format(val_acc, val_loss.data))
-
-
-if __name__ == "__main__":
-    num_class = 2
-    testmodel = pretrainedmodels.__dict__['resnext101_32x4d'](
-        num_classes=1000, pretrained='imagenet'
-    )
-
-    # last fully-connected linear module settings
-    testmodel.last_layer_name = 'last_linear'
-    num_feats = testmodel.last_linear.in_features
-    testmodel.last_linear = torch.nn.Linear(num_feats, num_class)
-
-    # last fully-connected linear module settings
-    testmodel.last_layer_name = 'last_linear'
-    num_feats = testmodel.last_linear.in_features
-    testmodel.last_linear = torch.nn.Linear(num_feats, num_class)
-    
-    # checkpoint path
-    checkpoint_path = os.path.join(
-        CHECKPOINT_DIR,
-        "aggresion_ResNeXt101_32x4d_split1_1.pth"
-    )
-
-    # load model checkpoint
-    checkpoint = torch.load(checkpoint_path)
-    testmodel.load_state_dict(checkpoint)
-
-    if torch.cuda.is_available():
-        testmodel.cuda()
-
-    # testing split
-    with torch.no_grad():
-        testmodel.eval()
-        
-        # 1 epoch over all testset
-        running_acc = 0.0
-        for i, (X, labels) in enumerate(train_loader):
-            if torch.cuda.is_available():
-                X = torch.autograd.Variable(X.cuda())
-                labels = torch.autograd.Variable(labels.cuda())
-            
-            # feed forward & get prediction
-            outputs = testmodel(X)
-            _, preds = torch.max(outputs.data, 1)
-            acc = torch.mean((preds == labels.data).float())
-            running_acc += acc
-
-        running_acc = running_acc/len(test_loader)
-        print("-- Test Split - Avg Test Acc: {: .4f}".format(running_acc))
