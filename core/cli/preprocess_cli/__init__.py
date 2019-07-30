867b63d5f61f96a37ec5dbdc0accb43ed9e6174b
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-07-30 14:47:28 +0800

    [CLI] support for video and img rank pooling

--- core/cli/preprocess_cli/__init__.py
+++ core/cli/preprocess_cli/__init__.py
@@ -11,56 +11,25 @@ import numpy as np
 # File utilities
 from core.utils.file_system import (
     safe_mkdir,
+    abs_listdir,
     search_files_recursively,
     get_basename,
     clean_filename
 )
 # Approximated Rank Pooling
 from core.cli.preprocess_cli.appx_rank_pooling import (
-    cvApproxRankPooling, Buffer
+    run_video_appx_rank_pooling,
+    run_img_appx_rank_pooling
 )
 video_extensions = ['.avi', '.mp4', '.webm', '.mov', '.mkv']
 
 
 """
-APPROXIMATED RANK POOLING
+VIdeo APPROXIMATED RANK POOLING
 
-Apply approximated rank pooling algorithm to convert video frames
-into a video representation
+Apply approximated rank pooling algorithm to convert video
+into rank pooled frames representing the action in video
 """
-
-def run_appx_rank_pooling(
-    video_path,
-    outdir,
-    buffer_size=10,
-    img_name_tmpl='img_{:05d}.png'
-):
-    """Run ARP function.
-    """
-
-    safe_mkdir(outdir)  # create directory for each video data
-
-    current = current_process()
-    cap = cv2.VideoCapture(video_path)
-    buffer = Buffer(buffer_size)
-    success = True
-
-    while success:
-        success, frame = cap.read()
-
-        if buffer.isfull():
-            frames = buffer.clear()
-            rank_pooled = cvApproxRankPooling(frames)
-            
-            image_name = img_name_tmpl.format(buffer.batch_count)
-            outpath = os.path.join(outdir, image_name)
-            
-            print(".. Writing to %s" % outpath)
-            cv2.imwrite(outpath, rank_pooled)
-
-        buffer.enqueue(frame)
-    cap.release()
-
 
 @click.command()
 @click.argument(
@@ -74,28 +43,29 @@ def run_appx_rank_pooling(
     type = click.Path(exists=False, dir_okay=True)
 )
 @click.option(
-    '--img_name_tmpl',
-    default = 'img_{:05d}.png',
-    type = click.STRING
-)
-@click.option(
     '-j',
     '--n_jobs',
     default = 8,
     type = int
 )
-def appx_rank_pooling(
+@click.option(
+    '-x',
+    '--img_ext',
+    default = '.jpg',
+    type = str
+)
+def video_appxRankPooling(
     source,
     dest,
-    img_name_tmpl,
-    n_jobs
+    n_jobs,
+    img_ext
 ):
     """
     Usage: 
-        > preprocess_cli appx_rank_pooling {YOUR_VIDEO_DIR} \ 
+        > preprocess_cli video-appx-rank-pooling {YOUR_VIDEO_DIR} \ 
             {YOUR_SAVE_FOLDER} [--OPTIONS] 
     """
-    print("Executing appx_rank_pool...")
+    print("Executing appx_rank_pool on video...")
     safe_mkdir(dest)
     
     for class_folder in os.listdir(source):     # run appx rank pool for each video in all class_folder
@@ -113,13 +83,82 @@ def appx_rank_pooling(
             os.path.join(outfolder, clean_filename(get_basename(video)))
             for video in video_files
         ]
+        img_exts = [img_ext]* len(outpaths)  # TODO: optimise this extension duplicating given every element is constant
 
         print(". Current class folder: %s, total:%d" %(class_folder, len(video_files)))
 
-        run_args = list(zip(video_files, outpaths))
-        Pool(n_jobs).starmap(
-            run_appx_rank_pooling, run_args
+        run_args = list(zip(video_files, outpaths, img_exts))
+        results = Pool(n_jobs).starmap(
+            run_video_appx_rank_pooling, run_args
         )
 
         print(". Finished %s." % class_folder)
 
+
+"""
+images APPROXIMATED RANK POOLING
+
+Apply approximated rank pooling algorithm to convert images/frames
+into rank pooling representation.
+"""
+
+@click.command()
+@click.argument(
+    'source',
+    envvar = 'SRC',
+    type = click.Path(exists=True, dir_okay=True)
+)
+@click.argument(
+    'dest',
+    envvar = 'SAVE_FOLDER',
+    type = click.Path(exists=False, dir_okay=True)
+)
+@click.option(
+    '-j',
+    '--n_jobs',
+    default = 8,
+    type = int
+)
+@click.option(
+    '-x',
+    '--img_ext',
+    default = '.jpg',
+    type = str
+)
+def imgs_appxRankPooling(
+    source,
+    dest,
+    n_jobs,
+    img_ext
+):
+    """
+    Usage: 
+        > preprocess_cli rgbs-appxrankpooling {YOUR_VIDEO_DIR} \ 
+            {YOUR_SAVE_FOLDER} [--OPTIONS] 
+    """
+    print("Executing rgbs-appx-rank-pooling on video...")
+    safe_mkdir(dest)
+    
+    source = os.path.abspath(source)
+    for class_folder in abs_listdir(source):     # run appx rank pool for each video in all class_folder
+        subdirs = abs_listdir(class_folder)
+        outdir = os.join.path(dest, get_basename(class_folder))
+        
+        safe_mkdir(outdir)
+
+        outpaths = [
+            os.path.join(outdir, get_basename(subdir))
+            for subdir in subdirs
+        ]
+
+        for outpath in outpaths:
+            safe_mkdir(outpath)
+
+        print(". Current class folder: %s, total:%d" %(class_folder, len(video_files)))
+
+        run_args = list(zip(subdir, outpaths, img_exts))
+        results = Pool(n_jobs).starmap(
+            run_img_appx_rank_pooling, run_args
+        )
+
+        print(". Finished %s." % class_folder)
